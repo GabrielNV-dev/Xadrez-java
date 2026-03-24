bf7124aaa9a48fc5cf24880a31af1e736d70dfad
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-03-24 11:58:51 -0300

    Validação da torre

---
+++ Xadrez/src/Torre.java
@@ -0,0 +1,24 @@
+public class Torre {
+    public static int validar(String[][] tabuleiro, String peca, int l, int c, int destinoL, int destinoC){
+
+    int limiteLmenos, limiteLmais, limiteCmenos, limiteCmais;
+
+    // Navegar pela matriz para achar os pontos limites.
+    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
+
+        if (tabuleiro[l + navegador][c] != "   "){
+            if (destinoL > navegador){
+                return 0;
+            }
+        }
+    }
+
+
+
+
+
+
+
+    return 1;
+    }
+}
