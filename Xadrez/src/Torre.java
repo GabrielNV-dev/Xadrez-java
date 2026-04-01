54e14ba6812381009fda129148e5ac01a29fc0ca
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-04-01 11:55:04 -0300

    Add files via upload

--- Xadrez/src/Torre.java
+++ Xadrez/src/Torre.java
@@ -1,27 +1,40 @@
 public class Torre {
     public static int validar(String[][] tabuleiro, String peca, int l, int c, int destinoL, int destinoC){
 
-    int limiteLmenos, limiteLmais, limiteCmenos, limiteCmais;
-
+    if ((c != destinoC && l != destinoL) || (c == destinoC || l == destinoL)){
+            return 0;
+        }
     // Navegar pela matriz para achar os pontos limites.
     for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
         if (tabuleiro[l + navegador][c] != "   "){
-            System.out.println(destinoL + " a " + navegador);
-            if (destinoL > navegador){
+            if (destinoL >= navegador){
                 return 0;
             }
         }
     }
 
-        for (int navegador = 1; navegador < l; navegador++){
+    for (int navegador = 1; navegador < l; navegador++){
             if (tabuleiro[l - navegador][c] != "   "){
-                System.out.println(destinoL + " a " + navegador);
-                if (destinoL < (l-navegador)){
+                if (destinoL <= (l-navegador)){
+                    return 0;
+                }
+            }
+    }
+
+    for (int navegador = 1; navegador < c; navegador++){
+            if (tabuleiro[l][c - navegador] != "   "){
+                if (destinoC <= (c-navegador)){
+                    return 0;
+                }
+            }
+    }
+    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
+            if (tabuleiro[l][c+navegador] != "   "){
+                if (destinoC >= navegador){
                     return 0;
                 }
             }
         }
-
     return 1;
     }
 }
