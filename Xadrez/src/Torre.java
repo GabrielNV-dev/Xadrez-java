80aa58ae535aef15623b000cbc4753a9705f6827
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-04-02 11:59:45 -0300

    Refactor Torre class validation method

--- Xadrez/src/Torre.java
+++ Xadrez/src/Torre.java
@@ -1,40 +1,164 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Torre {
     public static int validar(String[][] tabuleiro, String peca, int l, int c, int destinoL, int destinoC){
 
-    if ((c != destinoC && l != destinoL) || (c == destinoC || l == destinoL)){
+        String[] pretas = {" t ", " c ", "b< ", " d ", " r ", " >b", " c ", " t ", " p "};//Cria um vetor com as peças pretas
+        String[] brancas = {" T ", " C ", "B< ", " D ", " R ", " >B", " C ", " T ", " P "};//Cria um vetor com as peças brancas
+
+        List<String> pecas_pretas = Arrays.asList(pretas);//Cria uma lista com o vetor das peças pretas
+        List<String> pecas_brancas = Arrays.asList(brancas);//Cria uma lista com o vetor das peças brancas
+
+
+        if ((c != destinoC && l != destinoL) || (c == destinoC && l == destinoL)){
+            System.out.println(c + " lol " + destinoC);
             return 0;
-        }
-    // Navegar pela matriz para achar os pontos limites.
-    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
-        if (tabuleiro[l + navegador][c] != "   "){
-            if (destinoL >= navegador){
-                return 0;
-            }
-        }
-    }
+        }else{
 
-    for (int navegador = 1; navegador < l; navegador++){
-            if (tabuleiro[l - navegador][c] != "   "){
-                if (destinoL <= (l-navegador)){
-                    return 0;
-                }
-            }
-    }
+            if(pecas_brancas.contains(peca)){
 
-    for (int navegador = 1; navegador < c; navegador++){
-            if (tabuleiro[l][c - navegador] != "   "){
-                if (destinoC <= (c-navegador)){
-                    return 0;
+                if(pecas_pretas.contains(tabuleiro[destinoL][destinoC])){
+                    // Navegar pela matriz para achar os pontos limites.
+                    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
+                        if (tabuleiro[l + navegador][c] != "   "){
+                            if (destinoL > navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
+                        if (tabuleiro[l][c+navegador] != "   "){
+                            if (destinoC > navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < l; navegador++){
+                        if (tabuleiro[l - navegador][c] != "   "){
+                            if (destinoL < (l-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < c; navegador++){
+                        if (tabuleiro[l][c - navegador] != "   "){
+                            if (destinoC < (c-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
+                }else{
+                    // Navegar pela matriz para achar os pontos limites.
+                    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
+                        if (tabuleiro[l + navegador][c] != "   "){
+                            if (destinoL >= navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
+                        if (tabuleiro[l][c+navegador] != "   "){
+                            if (destinoC >= navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < l; navegador++){
+                        if (tabuleiro[l - navegador][c] != "   "){
+                            if (destinoL <= (l-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < c; navegador++){
+                        if (tabuleiro[l][c - navegador] != "   "){
+                            if (destinoC <= (c-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
+                }
+
+            }else{
+
+                if(pecas_brancas.contains(tabuleiro[destinoL][destinoC])){
+                    System.out.println("lol");
+                    // Navegar pela matriz para achar os pontos limites.
+                    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
+                        if (tabuleiro[l + navegador][c] != "   "){
+                            if (destinoL > navegador+2){
+                                System.out.println(navegador+3 + " E " + destinoL);
+                                return 10;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
+                        if (tabuleiro[l][c+navegador] != "   "){
+                            if (destinoC > navegador){
+                                return 20;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < l; navegador++){
+                        if (tabuleiro[l - navegador][c] != "   "){
+                            if (destinoL < (l-navegador)){
+                                return 30;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < c; navegador++){
+                        if (tabuleiro[l][c - navegador] != "   "){
+                            if (destinoC < (c-navegador)){
+                                return 40;
+                            }
+                        }
+                    }
+                }else{
+                    // Navegar pela matriz para achar os pontos limites.
+                    for (int navegador = 1; (l+navegador) <= destinoL; navegador++){
+                        if (tabuleiro[l + navegador][c] != "   "){
+                            if (destinoL >= navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
+                        if (tabuleiro[l][c+navegador] != "   "){
+                            if (destinoC >= navegador){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < l; navegador++){
+                        if (tabuleiro[l - navegador][c] != "   "){
+                            if (destinoL <= (l-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
+
+                    for (int navegador = 1; navegador < c; navegador++){
+                        if (tabuleiro[l][c - navegador] != "   "){
+                            if (destinoC <= (c-navegador)){
+                                return 0;
+                            }
+                        }
+                    }
                 }
             }
+
+        return 1;
     }
-    for (int navegador = 1; (c+navegador) <= destinoC; navegador++){
-            if (tabuleiro[l][c+navegador] != "   "){
-                if (destinoC >= navegador){
-                    return 0;
-                }
-            }
-        }
-    return 1;
-    }
-}
+}}
