1990c3b13ccadcbec25a171d181ab94afc428786
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-04-09 11:58:38 -0300

    Add files via upload

---
+++ Xadrez/src/Cavalo.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Cavalo {
+    public static int validar(String[][] tabuleiro, String peca, int l, int c, int destinoL, int destinoC){
+
+        String[] pretas = {" t ", " c ", "b< ", " d ", " r ", " >b", " c ", " t ", " p "};//Cria um vetor com as peças pretas
+        String[] brancas = {" T ", " C ", "B< ", " D ", " R ", " >B", " C ", " T ", " P "};//Cria um vetor com as peças brancas
+
+
+        List<String> pecas_pretas = Arrays.asList(pretas);//Cria uma lista com o vetor das peças pretas
+        List<String> pecas_brancas = Arrays.asList(brancas);//Cria uma lista com o vetor das peças brancas
+
+
+        if (pecas_brancas.contains(peca)){
+            System.out.println("dfew");
+            boolean v1 = l-2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
+            boolean v2 = l+2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
+            boolean v3 = c-2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
+            boolean v4 = c+2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
+
+            System.out.println(tabuleiro[destinoL][destinoC]);
+
+            if(!(v1 || v2 || v3 || v4) || pecas_brancas.contains(tabuleiro[destinoL][destinoC])){
+                System.out.println("lol");
+                return 0;
+            }
+
+        }
+        return 1;
+    }
+}
