ddf38c90980cd8c45e4a89a08a12a8a43b377290
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-04-14 09:56:45 -0300

    grafico ao xadrez
    Refactor visualization and input handling for chess game.

--- Xadrez/src/Main.java
+++ Xadrez/src/Main.java
@@ -1,16 +1,39 @@
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static final String reset = "\u001B[0m";
+    public static final String preto =  "\u001B[38;5;16m";
+    public static final String branco =  "\u001B[38;5;255m";
+    public static final String FUNDO_BRANCO = "\u001B[1;32;47m";
+    public static final String FUNDO_PRETO = "\u001B[48;5;22m";
 
     public static void vizualizar(String tabuleiro[][]) {
-        System.out.println("    1     2     3     4     5     6     7     8 ");
-        System.out.println("    |     |     |     |     |     |     |     |");
+        System.out.println("   1  2  3  4  5  6  7  8 ");
+        System.out.println("   |  |  |  |  |  |  |  |");
+
+
         for (int i = 0; i < tabuleiro.length; i++){
             System.out.print((i+1)+"-");
-            for (int t = 0; t < tabuleiro.length; t++){
-                System.out.print("(" + tabuleiro[i][t] + ") ");
+
+            for (int j = 0; j < tabuleiro.length; j++){
+
+                String peca = tabuleiro[i][j];
+
+                String fundo = ((i + j) % 2 == 0) ? FUNDO_BRANCO : FUNDO_PRETO;
+
+                if (peca.trim().isEmpty()) {
+                    System.out.print(fundo + "   " + reset );
+                }
+                else if (Character.isUpperCase(peca.trim().charAt(0))) {
+                    System.out.print(fundo + branco + peca + reset );
+                } else if (peca == " >B") {
+                    System.out.print(fundo + branco + peca + reset );
+                } else {
+                    System.out.print(fundo + preto + peca + reset);
+                }
             }
             System.out.println();
         }
@@ -27,11 +50,7 @@ public class Main {
         List<String> pecas_pretas = Arrays.asList(pretas);//Cria uma lista com o vetor das peças pretas
         List<String> pecas_brancas = Arrays.asList(brancas);//Cria uma lista com o vetor das peças brancas
 
-        // Uso de dois 'for' para imprimir o tabuleiro com base nas coordenadas
-        System.out.println("    1     2     3     4     5     6     7     8 ");
-        System.out.println("    |     |     |     |     |     |     |     | ");
         for (int i = 0; i < tabuleiro.length; i++) {
-            System.out.print((i+1)+"-");
 
             for (int t = 0; t < tabuleiro.length; t++) {
 
@@ -50,12 +69,10 @@ public class Main {
                 else{
                     tabuleiro[i][t] = "   ";// O resto dos espaços recebem três espaços, sendo o "vazio" do tabuleiro na visualização
                 }
-
-                System.out.print("(" + tabuleiro[i][t] + ") ");//Imprime o tabuleiro
             }
-            System.out.println();
-
         }
+
+        vizualizar(tabuleiro);
         System.out.println("Branco começa");
         while (true){
             //Pergunta para o usuário onde está a peça e para onde ele deseja ir
@@ -69,17 +86,22 @@ public class Main {
                 System.out.println("Coluna que deseja jogar:");
                 DestinoC = sc.nextInt()-1;
 
-                if(pecas_pretas.contains(tabuleiro[l][c]) && (turno % 2 == 0)){
-                    System.out.println("Jogue com as suas peças!");
-                    vizualizar(tabuleiro);
-                    l = 1000;
-                } else if (pecas_brancas.contains(tabuleiro[l][c]) && (turno % 2 == 1)) {
-                    System.out.println("Jogue com as suas peças!");
-                    vizualizar(tabuleiro);
+                try {
+                    if(pecas_pretas.contains(tabuleiro[l][c]) && (turno % 2 == 0)){
+                        System.out.println("Jogue com as suas peças!");
+                        vizualizar(tabuleiro);
+                        l = 1000;
+                    } else if (pecas_brancas.contains(tabuleiro[l][c]) && (turno % 2 == 1)) {
+                        System.out.println("Jogue com as suas peças!");
+                        vizualizar(tabuleiro);
+                        l = 1000;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Posição inválida!");
                     l = 1000;
                 }
 
-            } while (l < 0 || l > 7 || c <0 || c > 7 || DestinoL < 0 || DestinoL > 7 || DestinoC < 0 || DestinoC > 7 );
+            } while (l < 0 || l > 7 || c < 0 || c > 7 || DestinoL < 0 || DestinoL > 7 || DestinoC < 0 || DestinoC > 7 );
 
             System.out.println(gerenciador.adm(tabuleiro, tabuleiro[l][c], l, c, DestinoL, DestinoC));
             if (gerenciador.adm(tabuleiro, tabuleiro[l][c], l, c, DestinoL, DestinoC) == 1){
