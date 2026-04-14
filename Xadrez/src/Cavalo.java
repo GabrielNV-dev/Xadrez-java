0842101225565b3cd40705811575299b6225649c
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-04-14 08:37:12 -0300

    Finalização do cavalo

--- Xadrez/src/Cavalo.java
+++ Xadrez/src/Cavalo.java
@@ -11,22 +11,23 @@ public class Cavalo {
         List<String> pecas_pretas = Arrays.asList(pretas);//Cria uma lista com o vetor das peças pretas
         List<String> pecas_brancas = Arrays.asList(brancas);//Cria uma lista com o vetor das peças brancas
 
+        boolean v1 = l-2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
+        boolean v2 = l+2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
+        boolean v3 = c-2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
+        boolean v4 = c+2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
 
         if (pecas_brancas.contains(peca)){
-            System.out.println("dfew");
-            boolean v1 = l-2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
-            boolean v2 = l+2 == destinoL && (c + 1 == destinoC || c - 1 == destinoC);
-            boolean v3 = c-2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
-            boolean v4 = c+2 == destinoC && (l + 1 == destinoL || l - 1 == destinoL);
-
-            System.out.println(tabuleiro[destinoL][destinoC]);
 
             if(!(v1 || v2 || v3 || v4) || pecas_brancas.contains(tabuleiro[destinoL][destinoC])){
-                System.out.println("lol");
                 return 0;
             }
 
+        } else{
+            if(!(v1 || v2 || v3 || v4) || pecas_pretas.contains(tabuleiro[destinoL][destinoC])){
+                return 0;
+            }
         }
         return 1;
     }
 }
+a
